061e41b44f0885ba8a121cfbeee6d213bf382403
Gboyega Idowu <gboyegaidowu@Gboyegas-MBP.broadband>
2023-10-22 16:10:41 +0100

    Second Commit

--- formatScript.js
+++ formatScript.js
@@ -2,7 +2,7 @@ const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
 
-tabs.forEach(tab =>{
+tabs.forEach(tab => {
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
